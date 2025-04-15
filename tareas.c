311d398d49408cd15aca4f69ce115c58cf16e0af
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-04-15 14:47:13 -0300

    Bucle for iniciado

--- tareas.c
+++ tareas.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Tarea{
+typedef struct Tarea
+{
     int TareaID; //[1000, ... )
     char *Descripcion;
     int Duracion; // [10, 100]
 } Tarea;
 
-typedef struct Nodo{
+typedef struct Nodo
+{
     Tarea T;
-    Nodo * Siguiente;
+    Nodo *Siguiente;
 } Nodo;
 
-typedef Nodo * Lista;
+typedef Nodo *Lista;
 
-int main(){
+int main()
+{
 
-    
+    int id = 1000;
+    char opc = ' ';
+    char buffer[128];
+
+    while (1)
+    {
+        printf("Ingresa la tarea: ");
+        gets(buffer);
+
+        printf("\n¿Agregar mas tareas?");
+        scanf("%c", &opc);
+        getchar();
+        if (opc == 's' || opc == 'S' || opc == '1'){
+            id++;
+            continue;
+        }
+        break;
+    }
+
+    /*
+        1) Desarrolle una interfaz de carga de tareas para solicitar tareas pendientes, en la cual se
+        solicite descripción y duración de la misma (el id debe ser generado automáticamente por
+        el sistema, de manera autoincremental comenzando desde el número 1000). Al cabo de
+        cada tarea consulte al usuario si desea ingresar una nueva tarea o finalizar la carga.
+
+        2) Implemente una interfaz para elegir qué tareas de la lista de pendientes deben ser
+        transferidas a la lista de tareas realizadas.
+
+        3) Implemente una funcionalidad que permita listar todas las tareas pendientes y realizadas.
+
+        4) Implemente una funcionalidad que permita consultar tareas por id o palabra clave y
+        mostrarlas por pantalla, indicando si corresponde a una tarea pendiente o realizada.
+    */
+
     return 0;
 }
